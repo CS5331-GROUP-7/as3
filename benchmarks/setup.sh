9b7d7182a741e1fe6eae93620031ab5d5ee483fe
ZawLin <zawlin@zl.io>
2018-04-22 12:17:10 +0800

    * fix setup script

--- benchmarks/setup.sh
+++ benchmarks/setup.sh
@@ -3,4 +3,6 @@ sudo cp -Rvf sites-enabled /etc/apache2/sites-enabled
 cp -Rvf ssl /home/cs5331/Desktop
 sudo cp -Rvf www /var/www/
 sudo cp hosts /etc/hosts
+sudo a2enmod ssl
 sudo service apache2 restart
+
