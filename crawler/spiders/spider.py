8f3ba8a2cd2e277785f0dac512b1687a967fa775
ZawLin <zawlin@zl.io>
2018-04-23 22:15:57 +0800

    * handle select tag correctly

--- crawler/spiders/spider.py
+++ crawler/spiders/spider.py
@@ -14,11 +14,15 @@ def log(msg):
 def _retrieve_form_element(form, origin_url):
     fields = {}
     for x in form.inputs:
+
         if x.value is None:
             if type(x) == html.SelectElement:
                 x.value = x.value_options[0]
             else:
                 x.value = "None"
+
+        if type(x) == html.SelectElement:
+            fields[x.name]=x.value
         if type(x) == html.TextareaElement:
             fields[x.name] = [""]
         elif x.name and ("type" in x.keys() and x.type != "submit") and not ("Fatal error" in x.value):
